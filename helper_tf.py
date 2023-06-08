5f269ff20df1ca8ccefd9e4bb0154728ac013a57
forcoding1 <92193879+forcoding1@users.noreply.github.com>
2023-06-08 22:11:40 +0530

    helper functions for the tf series

---
+++ helper_tf.py
@@ -0,0 +1,9 @@
+from sklearn.metrics import precision_score, recall_score, f1_score, accuracy_score
+
+def results(y_true, y_pred):
+  """Takes true and predicted values and gives 4 evaluation metrics"""
+  accuracy_0 = accuracy_score(y_true, y_pred)
+  f1_score_0 = f1_score(y_true, y_pred)
+  recall_0 = recall_score(y_true, y_pred)
+  precision_0 = precision_score(y_true, y_pred)
+  return {"F1_Score" : f1_score_0, "Accuracy" : accuracy_0, "Precision": precision_0,"Recall": recall_0}
