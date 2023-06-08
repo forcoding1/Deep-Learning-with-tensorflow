4fa820b38b0e2b540bb8b61a1168ee30967f93a6
forcoding1 <92193879+forcoding1@users.noreply.github.com>
2023-06-08 22:26:33 +0530

    updated results function for multiclass

--- helper_tf.py
+++ helper_tf.py
@@ -4,8 +4,8 @@ def results(y_true, y_pred):
   """Takes true and predicted values and gives 4 evaluation metrics"""
   
   model_precision, model_recall, model_f1, _ = precision_recall_fscore_support(y_true, y_pred, average="weighted")
-  accuracy_0 = accuracy_score(y_true, y_pred)
-  f1_score_0 = f1_score(y_true, y_pred)
-  recall_0 = recall_score(y_true, y_pred)
-  precision_0 = precision_score(y_true, y_pred)
+  accuracy_0 = accuracy_score(y_true, y_pred) * 100
+  f1_score_0 = model_f1
+  recall_0 = model_recall
+  precision_0 = model_precision
   return {"F1_Score" : f1_score_0, "Accuracy" : accuracy_0, "Precision": precision_0,"Recall": recall_0}
